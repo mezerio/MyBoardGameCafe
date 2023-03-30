3d0e8ca93bdd40362cc8c6e7120707c6a74f096d
mezerio <maazvali@hotmail.co.uk>
2023-03-30 05:57:49 +0100

    initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+// ng serve to start on localhost
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header.component";
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "HeaderComponent",
+  template: "<h1>sm</h1>",
+})
+export class HeaderComponent {}
