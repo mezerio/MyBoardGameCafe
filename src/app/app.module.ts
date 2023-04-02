4b1de63fd3af7d1019492a0257e6848d92268bb1
mezerio <maazvali@hotmail.co.uk>
2023-04-02 09:31:53 +0100

    readme img Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,6 @@
 // ng g c [component name]
 // ng g s [service name]
 
-// npm install -g firebase-tools
-// firebase login
-// firebase init
-// instead of public make the file "dist/[name]"
-// ng build --configuration=production
-// firebase deploy
-
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
